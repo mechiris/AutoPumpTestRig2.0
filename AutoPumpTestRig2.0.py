a259adb4e90e1f0d50cdf40726d3f2bbb8280031
mechiris <ryan@sightmachine.com>
2015-10-25 14:45:33 -0700

    Additional commits before testing v0.1

--- AutoPumpTestRig2.0.py
+++ AutoPumpTestRig2.0.py
@@ -1,8 +1,8 @@
 from multiprocessing import Process, Pipe
 from Adafruit_PWM_Servo_Driver import PWM
 import datetime, time
-
-
+import subprocess as sbp
+import SimpleCV as scv
 
 ### Configureable parameters ###
 outputFile = 'run_{}.csv'.format(datetime.datetime.now().strftime("%Y_%m_%d-%H-%M-%S"))
@@ -16,7 +16,8 @@ breathTime = 0.5 # Time in seconds to take a breath
 sampleRate = 60 #how long to wait in seconds between 
 threshhold = 30 #threshold for ball detection 
 heightToMl = 10 #vertical axis pixel height to mL conversion factor
-
+saveImages = False
+imagesDir = 'imageoutput'
 
 def setServoPulse(channel, pulse):
 	pulseLength = 1000000                   # 1,000,000 us per second
@@ -45,20 +46,33 @@ def RunMotor(conn, breathTime, servoMin, servoMax):
 		conn.send(breathcounter)
 	conn.close()
 
-def processVision(threshold=30, heightToMl, saveImages):
-	#take a picture
+def processVision(threshold=30, heightToMl, saveImages, imagesDir):
+	#take a picture 
+	#this goes to disk because the uv4l direct to SCV solution doesn't work on jessie: http://www.linux-projects.org/modules/sections/index.php?op=viewarticle&artid=14
+	sbp.call(“raspistill -n -t 0 -o image.bmp, shell=True)
+	img = Image(“image.bmp”)
 
 	#find the ball
+	red_channel = img.splitChannels[0]
+	blobs = red_chanel.findBlobs(threshold)
+	ballheight = blobs[0].y
 
-	#compute 
+	#compute the mls 
+	mls = ballheight * heightToMl
+
+	#if save is activated 
 
 if __name__ == '__main__':
 	parent_conn, child_conn = Pipe()
 	p = Process(target=RunMotor, args=(child_conn,breathTime,servoMin,ServoMax))
 	p.start()
 
+	if saveImages:
+		if not os.path.exists(imagesDir):
+			os.makedirs(imagesDir)
+
 	while (True):
-		processVision(threshhold, heightToMl, saveImages)
+		processVision(threshhold, heightToMl, saveImages, imagesDir)
 		breathcount = parent_conn.recv()
 		
 	p.join()
